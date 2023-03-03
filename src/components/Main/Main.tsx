987d38608e1fd90e6d06b8d8467b8b3efddcb26f
Briuwu <millontebrian@gmail.com>
2023-03-03 20:22:35 +0800

    ADD the reviews section.

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import OffersSection from "./OffersSection";
+import ReviewsSection from "./ReviewsSection";
 
 const Main = () => {
   return (
     <main className="main">
       <Hero />
       <OffersSection />
+      <ReviewsSection />
     </main>
   );
 };
--- src/components/Main/ReviewsSection.tsx
+++ src/components/Main/ReviewsSection.tsx
@@ -1,7 +1,87 @@
-import React from "react";
+import { useState, useRef, useEffect, HTMLAttributes } from "react";
+import { motion } from "framer-motion";
+
+const reviewsArray = [
+  {
+    id: 1,
+    avatar: "./assets/images/avatar-anisha.png",
+    name: "Anisha Li",
+    review: `"Manage has supercharged our team's workflow. The ability to maintain visibility on larger milestone f all times keeps everyone motivated."`,
+  },
+  {
+    id: 2,
+    avatar: "./assets/images/avatar-ali.png",
+    name: "Ali Bravo",
+    review: `"We have been able to cancel so many others subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused."`,
+  },
+  {
+    id: 3,
+    avatar: "./assets/images/avatar-richard.png",
+    name: "Richard Watts",
+    review: `"Manage allows us to provide structure and process. It keeps us organized and focused. I can't stop recommending them to everyone I talk to!"`,
+  },
+  {
+    id: 4,
+    avatar: "./assets/images/avatar-shanai.png",
+    name: "Shanai Gough",
+    review: `"Their software allows us to tract, manage, and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive."`,
+  },
+];
 
 const ReviewsSection = () => {
-  return <div>ReviewsSection</div>;
+  const [selectedReview, setSelectedReview] = useState("Ali Bravo");
+  const [width, setWidth] = useState(0);
+  const carousel = useRef<HTMLDivElement>(null!);
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
+  const reviewToDisplay = reviewsArray.filter((el) => {
+    return el.name === selectedReview;
+  });
+
+  const slidesMobileElement = reviewToDisplay.map((el) => (
+    <li className="reviews__each">
+      <img src={el.avatar} alt="" />
+      <p className="name">{el.name}</p>
+      <p className="desc">{el.review}</p>
+    </li>
+  ));
+  const slidesDesktopElement = reviewsArray.map((el) => (
+    <motion.li className="reviews__each">
+      <img src={el.avatar} alt="" />
+      <p className="name">{el.name}</p>
+      <p className="desc">{el.review}</p>
+    </motion.li>
+  ));
+
+  const slidesSelection = reviewsArray.map((el) => (
+    <button
+      className={`reviews__selection-each ${
+        el.name === selectedReview ? "active" : ""
+      }`}
+      onClick={() => setSelectedReview(el.name)}
+    >
+      <span className="sr-only">{el.name}</span>
+    </button>
+  ));
+
+  return (
+    <section ref={carousel} className="reviews">
+      <h3>What they've said</h3>
+      <ul className="reviews__all mobile">{slidesMobileElement}</ul>
+      <motion.ul
+        drag="x"
+        dragConstraints={{ left: -width, right: 0 }}
+        whileTap={{ cursor: "grabbing" }}
+        className="reviews__all desktop"
+      >
+        {slidesDesktopElement}
+      </motion.ul>
+      <div className="reviews__selection">{slidesSelection}</div>
+    </section>
+  );
 };
 
 export default ReviewsSection;
